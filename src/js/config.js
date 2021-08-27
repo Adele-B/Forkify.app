f54ca4a653f2bddf8bfb96b84f2e66858ce3bceb
Adele-B <72464825+Adele-B@users.noreply.github.com>
2021-08-27 17:45:44 +0100

    Implemented add recipe functionality

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '605c7c96-8e74-4964-a3cf-7541f47fef98';
+export const MODAL_CLOSE_SEC = 2.5;
